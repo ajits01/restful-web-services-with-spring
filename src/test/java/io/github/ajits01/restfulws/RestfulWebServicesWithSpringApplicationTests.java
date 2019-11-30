fb298e00a2f0472d0a14ad74fbda530f91db41e2
Ajit <ajits01@outlook.com>
2019-11-30 15:42:37 +0530

    🎯  Initial Commit.
    Initial basic Project Structure.

---
+++ src/test/java/io/github/ajits01/restfulws/RestfulWebServicesWithSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.ajits01.restfulws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesWithSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
