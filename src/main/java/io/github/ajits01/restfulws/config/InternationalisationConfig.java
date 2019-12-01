82e63629f53eb478baa5ff61acaacb4d74f9eb91
Ajit <ajits01@outlook.com>
2019-12-02 00:23:43 +0530

    Add Internationalization (i18n) support

---
+++ src/main/java/io/github/ajits01/restfulws/config/InternationalisationConfig.java
@@ -0,0 +1,48 @@
+package io.github.ajits01.restfulws.config;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+@Configuration
+public class InternationalisationConfig implements WebMvcConfigurer {
+
+  @Bean
+  public LocaleResolver localeResolver() {
+    SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.US);
+    return localeResolver;
+  }
+  /*// Can be used for Accept-Language header based Locale changes
+    @Bean
+    public LocaleResolver acceptHeaderLocaleResolver() {
+      AcceptHeaderLocaleResolver acceptHeaderLocaleResolver = new AcceptHeaderLocaleResolver();
+      acceptHeaderLocaleResolver.setDefaultLocale(Locale.US);
+      return acceptHeaderLocaleResolver;
+    }
+  */
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+    localeChangeInterceptor.setParamName("lang");
+    return localeChangeInterceptor;
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(localeChangeInterceptor());
+  }
+
+  @Bean
+  public ResourceBundleMessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("messages");
+    return messageSource;
+  }
+}
--- src/main/java/io/github/ajits01/restfulws/controller/HelloWorldController.java
+++ src/main/java/io/github/ajits01/restfulws/controller/HelloWorldController.java
@@ -1,5 +1,8 @@
 package io.github.ajits01.restfulws.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +14,29 @@ import lombok.NoArgsConstructor;
 @RestController
 public class HelloWorldController {
 
+  @Autowired public MessageSource messageSource;
+
   @GetMapping(path = "/hello")
   public String hello(@RequestParam(name = "name", defaultValue = "World") String name) {
     return String.format("Hello %s.", name);
   }
 
+  @GetMapping(path = "/hello/{name}")
+  public String helloI18n(@PathVariable String name) {
+    String welcome = messageSource.getMessage("welcome", null, LocaleContextHolder.getLocale());
+    return String.format("%s %s.", welcome, name);
+  }
+
   @GetMapping(path = "/bean/hello/{name}")
   public HelloBean beanResource(@PathVariable String name) {
     return new HelloBean("Greetings ", name);
+  }
+
+  @GetMapping(path = "/i18n/hello/{name}")
+  public String helloI18nSupport(@PathVariable String name) {
+    String welcome = messageSource.getMessage("welcome", null, LocaleContextHolder.getLocale());
+    String greeting = messageSource.getMessage("greeting", null, LocaleContextHolder.getLocale());
+    return String.format("%s %s,\n%s", welcome, name, greeting);
   }
 }
 
