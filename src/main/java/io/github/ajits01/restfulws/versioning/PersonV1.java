0abd946fe511755928c02c11e6d1ed4f0f517316
Ajit <ajits01@outlook.com>
2019-12-02 15:31:15 +0530

    Explore Versioning Concepts

---
+++ src/main/java/io/github/ajits01/restfulws/versioning/PersonV1.java
@@ -0,0 +1,12 @@
+package io.github.ajits01.restfulws.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PersonV1 {
+  private String name;
+}
---
+++ src/main/java/io/github/ajits01/restfulws/versioning/PersonV2.java
@@ -0,0 +1,12 @@
+package io.github.ajits01.restfulws.versioning;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class PersonV2 {
+	private Name name;
+}
